c635a64dadf259e8abc8ef6ec0bf983496b0a3bd
BriceThu47400935 <brice.thu1@students.mq.edu.au>
2023-09-26 12:38:56 +1000

    Added Player Movement and Coins

---
+++ Week 8/Assets/Scripts/Coin.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        ScoreKeeper.Instance.IncreaseScore();
+        Destroy(gameObject);
+    }
+}
